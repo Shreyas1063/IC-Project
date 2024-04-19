e4d080f5f8d2cbbad1ed746ccea69b0b606dd863
Shreyas1063 <147465624+Shreyas1063@users.noreply.github.com>
2024-04-20 05:10:31 +0530

    Update man_management.c

--- man_management.c
+++ man_management.c
@@ -1,8 +1,18 @@
 #include "man_management.h"
 #include <stdio.h>
+#include <time.h>
 
 void log_entry_exit(char* student_name, char* room_number, char* action) {
-    // Log the entry or exit of a student into or from their room
+    // Get current date and time
+    time_t raw_time;
+    struct tm *timeinfo;
+    char timestamp[50];
+
+    time(&raw_time);
+    timeinfo = localtime(&raw_time);
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", timeinfo);
+
+    // Log the entry or exit of a student into or from their room with date and time
     FILE *log_file;
     log_file = fopen("student_logs.txt", "a"); // Open the file in append mode
     
@@ -11,7 +21,7 @@ void log_entry_exit(char* student_name, char* room_number, char* action) {
         return;
     }
     
-    fprintf(log_file, "%s %s room %s\n", student_name, action, room_number);
+    fprintf(log_file, "%s %s room %s at %s\n", student_name, action, room_number, timestamp);
     fclose(log_file);
 }
 
