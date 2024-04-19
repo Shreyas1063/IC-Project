768042ce15f7411f4fa8d19d8c38d1edd872a3e7
Shreyas1063 <147465624+Shreyas1063@users.noreply.github.com>
2024-04-20 05:19:32 +0530

    Create inventory_management.h

---
+++ inventory_management.h
@@ -0,0 +1,8 @@
+#ifndef INVENTORY_MANAGEMENT_H
+#define INVENTORY_MANAGEMENT_H
+
+// Function prototypes
+void issue_inventory_item(char* student_name, char* item);
+void display_inventory();
+
+#endif /* INVENTORY_MANAGEMENT_H */
