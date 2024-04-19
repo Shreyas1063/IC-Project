6a11786511a1b78d778795021c917a62fb62fea7
Vyom1111 <118009657+Vyom1111@users.noreply.github.com>
2024-04-20 03:35:34 +0530

    Update room_management.h

--- room_management.h
+++ room_management.h
@@ -3,12 +3,7 @@
 
 // Include necessary header files
 //#include "main.h"
-typedef struct {
-    char number[MAX_ROOM_NUMBER_LENGTH];
-    int capacity;
-    int occupancy;
-    char roommates[MAX_ROOM_CAPACITY][MAX_NAME_LENGTH]; // Assuming each roommate name has a maximum length
-} Room;
+
 
 // Function declarations
 Room* initializeRoomData(int numRooms);
