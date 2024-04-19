cc0d40f960e04e9751ab712afe6cc59a02f5bce4
Shreyas1063 <147465624+Shreyas1063@users.noreply.github.com>
2024-04-20 04:48:09 +0530

    Update man_management.h

--- man_management.h
+++ man_management.h
@@ -1,1 +1,9 @@
+#ifndef MAN_MANAGEMENT_H
+#define MAN_MANAGEMENT_H
+
+// Function prototypes
+void enter_room(char* student_name, char* room_number);
+void leave_room(char* student_name, char* room_number);
+
+#endif /* MAN_MANAGEMENT_H */
 
