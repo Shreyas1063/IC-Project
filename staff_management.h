5e4dd004f670ee06a2bf9e2e0286209e8aa45aa8
Shreyas1063 <147465624+Shreyas1063@users.noreply.github.com>
2024-04-20 05:33:00 +0530

    Create staff_management.h

---
+++ staff_management.h
@@ -0,0 +1,8 @@
+#ifndef STAFF_MANAGEMENT_H
+#define STAFF_MANAGEMENT_H
+
+// Function prototypes
+void add_staff(char* staff_type, char* name, char* mobile_number, int on_holiday);
+void display_staff();
+
+#endif /* STAFF_MANAGEMENT_H */
